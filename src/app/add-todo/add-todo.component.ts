7ee8f93eabcbf5a02a9ce7162999f17f79e66baa
Ruben <gotze19ariel@gmail.com>
2023-05-15 20:35:19 +0200

    creacion de modelo todo, formulario todo, y metodo usuando formgroup

--- src/app/add-todo/add-todo.component.ts
+++ src/app/add-todo/add-todo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../model/todo.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-todo',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTodoComponent implements OnInit {
 
-  constructor() { }
+  //propiedad para guardar nuestra data
+  saveTodo: Todo [] = [];
+  categoryTodo:string[] = ['Otros','Iglesia','Trabajo','Casa','Familia'];
+
+  //creamos nuestra propiedad para trabajar nuestro formulario reactivo
+  todoForm!:FormGroup;
+
+  constructor(private readonly formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    //creamos nuestra instancia del formulario para trabajar el html
+    this.todoForm = this.initForm();
+    this.onPatchValue();
   }
 
+  createTodo(){
+    console.log(this.todoForm.value);
+    this.saveTodo.push(this.todoForm.value)
+  }
+   //metodo para asignar o cargar datos en el formulario
+   onPatchValue(){
+    this.todoForm.patchValue({
+      complete:false,
+      category:'otros'
+    })
+  }
+  initForm(){
+    //aqui pasamos nuestros campos del formulario y los retornamos
+    return this.formBuilder.group({
+      //validamos los campos desde aqui
+      name:['',[Validators.required,Validators.minLength(5)]],
+      completed:'',
+      category:[''],
+      comments:['',[Validators.required]],
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AddTodoComponent } from './add-todo/add-todo.component';
 import { ListTodoComponent } from './list-todo/list-todo.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,10 @@ import { ListTodoComponent } from './list-todo/list-todo.component';
     ListTodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
